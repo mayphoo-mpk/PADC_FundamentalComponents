0290c5bb0b686239e67a6f313fd9e1ca99abbb7c
DESKTOP-13A358L\User <mayphoo.mpk11@gmail.com>
2017-11-04 17:48:54 +0630

    Initialize skeleton projects with default structure/value.

---
+++ app/src/main/java/mayphoo/mpk/sfc/SFCNewsApp.java
@@ -0,0 +1,18 @@
+package mayphoo.mpk.sfc;
+
+import android.app.Application;
+
+/**
+ * Created by User on 11/4/2017.
+ * This class represents application component.
+ */
+
+public class SFCNewsApp extends Application {
+
+    public static final String LOG_TAG = "SFCNewsApp"; //constant variable
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/mayphoo/mpk/sfc/activities/NewsListActivity.java
@@ -0,0 +1,31 @@
+package mayphoo.mpk.sfc.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import mayphoo.mpk.sfc.R;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
